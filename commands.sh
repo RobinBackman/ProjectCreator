da37969e0f6c93c9de47359e8a7d54b7c67c5c67
= <=>
2019-05-31 18:25:10 +0200

    Added Description.

--- commands.sh
+++ commands.sh
@@ -4,7 +4,7 @@ function newproject() {
     cd
     mkdir Development/$1
     cd Development/$1
-    curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":"'$1'", "private":true}' https://api.github.com/user/repos
+    curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":"'$1'", "description":"'$2'", "private":true}' https://api.github.com/user/repos
     git init
     git remote add origin https://github.com/RobinBackman/$1.git
     touch README.md
